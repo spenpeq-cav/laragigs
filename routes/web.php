ef2c89fed223d5123fa57197ae5b496f09136021
Spencer Pequegnat <spencer.peq@gmail.com>
2022-05-17 17:59:47 -0700

    Example routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return response('<h1>Hello world</h1>', 404)
+    ->header('Content-type', 'text/plain')
+    ->header('foo','bar');
+});
+
+Route::get('/posts/{id}', function($id){
+    dd($id);
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function(Request $request){
+    return $request->name . ' ' . $request->city;
 });
