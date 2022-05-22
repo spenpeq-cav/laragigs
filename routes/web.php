994a3294cc013223cf1da1179e458139f3355635
Spencer Pequegnat <spencer.peq@gmail.com>
2022-05-22 15:44:07 -0700

    Single listing format, Route model binding, components

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,10 @@ Route::get('/', function () {
     ]);
 });
 
-//Single listing
-Route::get('/listings/{id}', function ($id) {
+//Single listing, Route model binding
+Route::get('/listings/{listing}', function (Listing $listing) {
     return view('listing', [
-        'listing' => Listing::find($id)
+        'listing' => $listing
     ]);
 });
 
